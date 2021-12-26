517e36361906066951da5c794ca96efa4ae2e751
fsnopko <96683303+fsnopko@users.noreply.github.com>
2021-12-26 11:33:12 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Otvoriť túto stránku na [https://fsnopko.github.io/musicbit/](https://fsnopko.github.io/musicbit/)
+
+## Použiť ako rozšírenie
+
+Tento odkladací priestor možno pridať ako **rozšírenie** v aplikácii MakeCode.
+
+* otvoriť [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* kliknite na **Nový projekt**
+* kliknite na **Rozšírenia** v ponuke ozubeného kolieska
+* vyhľadať **https://github.com/fsnopko/musicbit** a importovať
+
+## Upraviť tento projekt ![Odznak stavu zostavy](https://github.com/fsnopko/musicbit/workflows/MakeCode/badge.svg)
+
+Ak chcete upraviť tento odkladací priestor v aplikácii MakeCode.
+
+* otvoriť [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* kliknite na **Importovať** a potom na **Importovať adresu URL**
+* prilepte **https://github.com/fsnopko/musicbit** a kliknite na tlačidlo importu
+
+## Ukážka blokov
+
+Tento obrázok zobrazuje kód blokov z posledného potvrdenia v hlavnej časti.
+Obnovenie tohto obrázka môže trvať niekoľko minút.
+
+![Vykreslené zobrazenie blokov](https://github.com/fsnopko/musicbit/raw/master/.github/makecode/blocks.png)
+
+#### Metadáta (používané na vyhľadávanie, vykresľovanie)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,268 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showString("Music:Bit by Filip Snopko")
+    basic.showString("Serial nm.:" + control.deviceSerialNumber())
+    basic.showString("Name:" + control.deviceName())
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    basic.showString("Netras ma!!!")
+    basic.showIcon(IconNames.Confused)
+    for (let index = 0; index < 6; index++) {
+        basic.showLeds(`
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            . . . . #
+            `)
+        basic.showLeds(`
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            . . . # #
+            `)
+        basic.showLeds(`
+            . . # # #
+            . . # # #
+            . . # # #
+            . . # # #
+            . . # # #
+            `)
+        basic.showLeds(`
+            . # # # #
+            . # # # #
+            . # # # #
+            . # # # #
+            . # # # #
+            `)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            # # # # .
+            # # # # .
+            # # # # .
+            # # # # #
+            # # # # .
+            `)
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            # # # # .
+            # # # . #
+            `)
+        basic.showLeds(`
+            # # . . .
+            # # . # .
+            # # . . .
+            # # # . .
+            # # . # #
+            `)
+        basic.showLeds(`
+            # . . . .
+            # . # . #
+            # . . . .
+            # # . . .
+            # . # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . # . .
+            . . . . .
+            . . . # .
+            # # # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . # . .
+            # # . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            . . . . .
+            . # . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showIcon(IconNames.Heart)
+        basic.showString("WOW")
+    }
+    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Databaza on.")
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.No)
+    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Confused)
+    basic.showIcon(IconNames.Angry)
+    basic.showIcon(IconNames.Asleep)
+    basic.showIcon(IconNames.Surprised)
+    basic.showIcon(IconNames.Silly)
+    basic.showIcon(IconNames.Fabulous)
+    basic.showIcon(IconNames.Meh)
+    basic.showIcon(IconNames.TShirt)
+    basic.showIcon(IconNames.Rollerskate)
+    basic.showIcon(IconNames.Duck)
+    basic.showIcon(IconNames.House)
+    basic.showIcon(IconNames.Tortoise)
+    basic.showIcon(IconNames.Butterfly)
+    basic.showIcon(IconNames.StickFigure)
+    basic.showIcon(IconNames.Ghost)
+    basic.showIcon(IconNames.Sword)
+    basic.showIcon(IconNames.Giraffe)
+    basic.showIcon(IconNames.Skull)
+    basic.showIcon(IconNames.Umbrella)
+    basic.showIcon(IconNames.Snake)
+    basic.showIcon(IconNames.Rabbit)
+    basic.showIcon(IconNames.Cow)
+    basic.showIcon(IconNames.QuarterNote)
+    basic.showIcon(IconNames.EigthNote)
+    basic.showIcon(IconNames.Pitchfork)
+    basic.showIcon(IconNames.Target)
+    basic.showIcon(IconNames.Triangle)
+    basic.showIcon(IconNames.LeftTriangle)
+    basic.showIcon(IconNames.Chessboard)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Scissors)
+    basic.showString("End")
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.clearScreen()
+    basic.showString("Pustim pesnicku.")
+    basic.pause(1000)
+    basic.showIcon(IconNames.EigthNote)
+    music.setBuiltInSpeakerEnabled(true)
+    soundExpression.giggle.play()
+    music.playMelody("C5 G A G A F D G ", 303)
+    music.playMelody("C5 G A G A F D G ", 303)
+    music.playMelody("A G F A G F F B ", 303)
+    music.playMelody("A G F A G F F B ", 303)
+    music.playMelody("A G F A G F F B ", 303)
+    soundExpression.giggle.play()
+    basic.showString("Koniec.")
+    basic.showIcon(IconNames.Happy)
+})
+music.setVolume(255)
+basic.showString("Ahoj!")
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # # . # #
+    # . . . #
+    . # # # .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . # . . .
+    # # # # #
+    . # . . .
+    . . # . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    # # # # #
+    # . . . #
+    # . . . #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . . . # .
+    # # # # #
+    . . . # .
+    . . # . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # # # . .
+    # . . # .
+    # # # . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # # # # #
+    . # . # .
+    . . # . .
+    `)
+basic.pause(1000)
+basic.showIcon(IconNames.Happy)
